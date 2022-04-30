2f656a3173b9149748ff100dfc881cffc6f40e5a
PontoReZZ <36677504+PontoReZZ@users.noreply.github.com>
2022-04-30 14:16:33 +0300

    Add files via upload

--- 01_autoplay.js
+++ 01_autoplay.js
@@ -2,8 +2,8 @@ if (window.location.toString().match(/https\:\/\/radio.yandex.ru/ig)) {
 
 	function change_track() {
 		try {
-			playing = document.querySelector('.progress__shape.progress__shape_bg > path').getAttribute('d').split(' ')[5];
-			void(playing == '1,0' && document.querySelector('[title="Играть [P]"]').click());
+			playing = document.querySelector('.progress__shape.progress__shape_play > path').getAttribute('d').split(' ')[6].split(',')[0];
+			void(playing == '34.020' && document.querySelector('[title="Играть [P]"]').click());
 		} catch(err) {};
 	};
 	setInterval(change_track, 1000);
