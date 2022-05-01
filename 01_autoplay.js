0edf92b83a0572a3ece474251183f43189db4952
PontoReZZ <36677504+PontoReZZ@users.noreply.github.com>
2022-05-01 22:36:19 +0300

    add button for switching autoplay

--- 01_autoplay.js
+++ 01_autoplay.js
@@ -1,11 +1,23 @@
 if (window.location.toString().match(/https\:\/\/radio.yandex.ru/ig)) {
 
+	var icon_autoplay = document.createElement('span');
+	icon_autoplay.className = 'icon autoplay';
+	icon_autoplay.title = 'Автовоспроизведение';
+	document.getElementsByClassName('head__right')[0].prepend(icon_autoplay);
+
 	function change_track() {
 		try {
 			playing = document.querySelector('.progress__shape.progress__shape_play > path').getAttribute('d').split(' ')[6].split(',')[0];
-			void(playing == '34.020' && document.querySelector('[title="Играть [P]"]').click());
-		} catch(err) {};
-	};
-	setInterval(change_track, 1000);
+			void(playing == '34.020' && document.querySelector('[title="Играть [P]"]').click())
+		} catch(err) {}
+	}
+
+	window.onload = autoplay_checking; 
+	document.querySelector('.autoplay').onclick = autoplay_checking;
+	function autoplay_checking() {
+		document.querySelector('.autoplay').classList.toggle('autoplay_enabled');
+		autoplay_enabled_el = document.querySelector('.autoplay').getAttribute('class').split(' ')[2];
+		autoplay_enabled_el == 'autoplay_enabled' ? autoplay_enabled = setInterval(change_track, 1000) : clearInterval(autoplay_enabled)
+	}
 
 }
