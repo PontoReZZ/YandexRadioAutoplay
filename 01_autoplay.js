a244bd90d8036d6f90511a2fade415e54816cc18
PontoReZZ <36677504+PontoReZZ@users.noreply.github.com>
2022-05-02 00:46:29 +0300

    add chrome.storage

--- 01_autoplay.js
+++ 01_autoplay.js
@@ -1,5 +1,15 @@
 if (window.location.toString().match(/https\:\/\/radio.yandex.ru/ig)) {
 
+	chrome.storage.sync.get(['key'], function(result) {
+		if (result.key == '1') {
+			document.querySelector('.autoplay').className = 'icon autoplay autoplay_enabled'
+		} else if (result.key == '0') {
+			document.querySelector('.autoplay').className = 'icon autoplay'
+		} else {
+			window.onload = autoplay_checking
+		}
+	})
+
 	var icon_autoplay = document.createElement('span');
 	icon_autoplay.className = 'icon autoplay';
 	icon_autoplay.title = 'Автовоспроизведение';
@@ -12,12 +22,23 @@ if (window.location.toString().match(/https\:\/\/radio.yandex.ru/ig)) {
 		} catch(err) {}
 	}
 
-	window.onload = autoplay_checking; 
 	document.querySelector('.autoplay').onclick = autoplay_checking;
 	function autoplay_checking() {
 		document.querySelector('.autoplay').classList.toggle('autoplay_enabled');
 		autoplay_enabled_el = document.querySelector('.autoplay').getAttribute('class').split(' ')[2];
-		autoplay_enabled_el == 'autoplay_enabled' ? autoplay_enabled = setInterval(change_track, 1000) : clearInterval(autoplay_enabled)
+		if (autoplay_enabled_el == 'autoplay_enabled') {
+			autoplay_enabled = setInterval(change_track, 1000);
+			is_auto = '1'
+		} else {
+			try {
+				clearInterval(autoplay_enabled);
+			} catch(err) {}
+			is_auto = '0'
+		}
+		
+	chrome.storage.sync.set({key: is_auto}, function() {
+	});
+
 	}
 
 }
